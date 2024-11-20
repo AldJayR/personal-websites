dc9bb21f14ff4fb422d17f8694ceddefaccaf5e4
AldJayR <aldwinjzs3@gmail.com>
2024-11-20 14:21:56 +0800

    Added services section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero";
+import ServicesSection from "../components/ServiceSection";
 
 const Home = () => {
     return (
         <>
             <Hero />
+            <ServicesSection />
         </>
     )
 }
