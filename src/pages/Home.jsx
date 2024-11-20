7aa442c29bdfabb14e6fd1d54d89d3ed4bfe6f60
AldJayR <aldwinjzs3@gmail.com>
2024-11-20 23:28:11 +0800

    added date restriction

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,17 @@ import ServicesSection from "../components/ServiceSection";
 import { AboutMe } from "../components/AboutMe";
 
 const Home = () => {
+    /*
+    const today = new Date().toISOString().split("T")[0];
+
+    const presentationDate = "2024-12-04";
+
+    if (today !== presentationDate) {
+        return <h1 className="text-center text-4xl font-roboto">It&apos;s not presentation day!</h1>
+    }
+    */
+    
+
     return (
         <>
             <Hero />
